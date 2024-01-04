eee94b627ffee7e87f23e79e12a101471edabdb5
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-01-04 16:07:21 +0100

    Delete demo stuff, connect to backend (#2)

--- plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,38 +1,47 @@
-import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
-import {
-  InfoCard,
-  Header,
-  Page,
-  Content,
-  ContentHeader,
-  HeaderLabel,
-  SupportButton,
-} from '@backstage/core-components';
-import { ExampleFetchComponent } from '../ExampleFetchComponent';
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { Content, ContentHeader, Header, HeaderLabel, InfoCard, Page, Progress, SupportButton } from "@backstage/core-components";
+import { fetchApiRef, useApi } from "@backstage/core-plugin-api";
+import useAsync from "react-use/lib/useAsync";
+import { Table } from "@backstage/core-components";
 
-export const ExampleComponent = () => (
-  <Page themeId="tool">
-    <Header title="Welcome to ros!" subtitle="Optional subtitle">
-      <HeaderLabel label="Owner" value="Team X" />
-      <HeaderLabel label="Lifecycle" value="Alpha" />
-    </Header>
-    <Content>
-      <ContentHeader title="Plugin title">
-        <SupportButton>A description of your plugin goes here.</SupportButton>
-      </ContentHeader>
-      <Grid container spacing={3} direction="column">
-        <Grid item>
-          <InfoCard title="Information card">
-            <Typography variant="body1">
-              All content should be wrapped in a card like this.
-            </Typography>
-          </InfoCard>
+export const ExampleComponent = () => {
+  const { fetch } = useApi(fetchApiRef);
+  const { value, loading } = useAsync(
+    async (): Promise<String> => fetch("http://localhost:8080/api/helloworld").then((response) => response.text())
+  );
+  if (loading) return <Progress />;
+  return (
+    <Page themeId="tool">
+      <Header title="Welcome to ros!" subtitle="Optional subtitle">
+        <HeaderLabel label="Owner" value="Team X" />
+        <HeaderLabel label="Lifecycle" value="Alpha" />
+      </Header>
+      <Content>
+        <ContentHeader title="Risiko- og sikkerhetsanalyse">
+          <SupportButton>Kul plugin ass!</SupportButton>
+        </ContentHeader>
+        <Grid container spacing={3} direction="column">
+          <Grid item>
+            <InfoCard title="Message from backend">
+              <Typography variant="body1">
+                {value ?? "Loading..."}
+              </Typography>
+            </InfoCard>
+          </Grid>
+          <Grid item>
+            <Table
+              columns={[
+                { title: "Col 1" },
+                { title: "Col 2" }
+              ]}
+              data={[
+
+              ]}
+            />
+          </Grid>
         </Grid>
-        <Grid item>
-          <ExampleFetchComponent />
-        </Grid>
-      </Grid>
-    </Content>
-  </Page>
-);
+      </Content>
+    </Page>
+  );
+};
