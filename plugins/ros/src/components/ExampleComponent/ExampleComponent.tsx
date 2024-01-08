257627b58f3fe403f854a446994707f94cb2511c
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-01-08 12:07:49 +0100

    Display name of user

--- plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { Content, ContentHeader, Header, HeaderLabel, InfoCard, Page, Progress, SupportButton } from "@backstage/core-components";
-import { fetchApiRef, useApi } from "@backstage/core-plugin-api";
+import { fetchApiRef, identityApiRef, ProfileInfo, useApi } from "@backstage/core-plugin-api";
 import useAsync from "react-use/lib/useAsync";
 import { Table } from "@backstage/core-components";
 
 export const ExampleComponent = () => {
   const { fetch } = useApi(fetchApiRef);
-  const { value, loading } = useAsync(
-    async (): Promise<String> => fetch("http://localhost:8080/api/helloworld").then((response) => response.text())
-  );
+  const {  value: data, loading } = useAsync(async (): Promise<String> => fetch("http://localhost:8080/api/helloworld").then((response) => response.text()));
+
+  const identityApi = useApi(identityApiRef);
+  const { value: user } = useAsync(async (): Promise<ProfileInfo> => identityApi.getProfileInfo());
+
+  const username = user?.displayName
   if (loading) return <Progress />;
   return (
     <Page themeId="tool">
@@ -23,9 +26,12 @@ export const ExampleComponent = () => {
         </ContentHeader>
         <Grid container spacing={3} direction="column">
           <Grid item>
+            <InfoCard><Typography>Heisann, {username ?? ""}!</Typography></InfoCard>
+          </Grid>
+          <Grid item>
             <InfoCard title="Message from backend">
               <Typography variant="body1">
-                {value ?? "Loading..."}
+                {data ?? "Loading..."}
               </Typography>
             </InfoCard>
           </Grid>
