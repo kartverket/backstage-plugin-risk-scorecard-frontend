de0dc52c2755fc5eefb3d964c2370f6a59fbba7c
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-01-09 14:05:40 +0100

    Fetch ROS from backend (#4)

--- plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/ros/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Typography } from "@material-ui/core";
-import { Content, ContentHeader, Header, HeaderLabel, InfoCard, Page, Progress, SupportButton } from "@backstage/core-components";
-import { fetchApiRef, identityApiRef, ProfileInfo, useApi } from "@backstage/core-plugin-api";
+import {
+  Content,
+  ContentHeader,
+  Header,
+  HeaderLabel,
+  InfoCard,
+  Page,
+  SupportButton,
+  Table
+} from "@backstage/core-components";
+import { fetchApiRef, githubAuthApiRef, ProfileInfo, useApi } from "@backstage/core-plugin-api";
 import useAsync from "react-use/lib/useAsync";
-import { Table } from "@backstage/core-components";
 
 export const ExampleComponent = () => {
+
+  const githubApi = useApi(githubAuthApiRef);
+  const { value: token } = useAsync(async (): Promise<string> => githubApi.getAccessToken("repo"));
+  const { value: profile } = useAsync(async (): Promise<ProfileInfo | undefined> => githubApi.getProfile());
+
   const { fetch } = useApi(fetchApiRef);
-  const {  value: data, loading } = useAsync(async (): Promise<String> => fetch("http://localhost:8080/api/helloworld").then((response) => response.text()));
+  const [roses, setRoses] = useState<string[]>();
 
-  const identityApi = useApi(identityApiRef);
-  const { value: profile } = useAsync(async (): Promise<ProfileInfo> => identityApi.getProfileInfo());
+  useEffect(() => {
+    if (token) {
+      fetch(`http://localhost:8080/api/github/${token}`)
+        .then((response) => response.json())
+        .then((data) => setRoses(data));
+    }
+  }, [token]);
 
-  if (loading) return <Progress />;
   return (
     <Page themeId="tool">
       <Header title="Welcome to ros!" subtitle="Optional subtitle">
@@ -26,13 +43,9 @@ export const ExampleComponent = () => {
         <Grid container spacing={3} direction="column">
           <Grid item>
             <InfoCard><Typography>Heisann, {profile?.displayName ?? ""}!</Typography></InfoCard>
-          </Grid>
-          <Grid item>
-            <InfoCard title="Message from backend">
-              <Typography variant="body1">
-                {data ?? "Loading..."}
-              </Typography>
-            </InfoCard>
+            <InfoCard><Typography>
+              <pre>{JSON.stringify(roses, null, 2) ?? ""}</pre>
+            </Typography></InfoCard>
           </Grid>
           <Grid item>
             <Table
@@ -40,9 +53,7 @@ export const ExampleComponent = () => {
                 { title: "Col 1" },
                 { title: "Col 2" }
               ]}
-              data={[
-
-              ]}
+              data={[]}
             />
           </Grid>
         </Grid>
