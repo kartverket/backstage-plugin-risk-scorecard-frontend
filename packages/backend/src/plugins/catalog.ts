b18c5ed4445dacd119c5df834c5dc9ca3a8062a1
marensofier <marensofieringsby@gmail.com>
2024-01-17 14:09:54 +0100

    feat: begynn på å bruke github for discovery av entiteter

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,14 +1,33 @@
-import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
-import { Router } from 'express';
-import { PluginEnvironment } from '../types';
+import {CatalogBuilder} from '@backstage/plugin-catalog-backend';
+import {ScaffolderEntitiesProcessor} from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
+import {Router} from 'express';
+import {PluginEnvironment} from '../types';
+import {defaultUserTransformer, GithubOrgEntityProvider} from "@backstage/plugin-catalog-backend-module-github";
 
 export default async function createPlugin(
-  env: PluginEnvironment,
+    env: PluginEnvironment,
 ): Promise<Router> {
-  const builder = await CatalogBuilder.create(env);
-  builder.addProcessor(new ScaffolderEntitiesProcessor());
-  const { processingEngine, router } = await builder.build();
-  await processingEngine.start();
-  return router;
+    const builder = await CatalogBuilder.create(env);
+    builder.addProcessor(new ScaffolderEntitiesProcessor());
+    const githubOrgProvider = GithubOrgEntityProvider.fromConfig(env.config, {
+        id: 'production',
+        orgUrl: 'https://github.com/bekk',
+        logger: env.logger,
+        schedule: env.scheduler.createScheduledTaskRunner({
+            frequency: {minutes: 60},
+            timeout: {minutes: 15},
+        }),
+        userTransformer: async (user, ctx) => {
+            const entity = await defaultUserTransformer(user, ctx);
+            if (entity && user.organizationVerifiedDomainEmails?.length) {
+                entity.spec.profile!.email = user.organizationVerifiedDomainEmails[0]
+            }
+            return entity;
+        }
+    })
+    builder.addEntityProvider(githubOrgProvider);
+
+    const {processingEngine, router} = await builder.build();
+    await processingEngine.start();
+    return router;
 }
