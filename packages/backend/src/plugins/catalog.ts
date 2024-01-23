1c4bffe58cb83e0ca74095b0963bf963ea107928
marensofier <marensofieringsby@gmail.com>
2024-01-23 14:05:39 +0100

    feat: legg til at man kan bruke github som provider for entities

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,7 +2,11 @@ import {CatalogBuilder} from '@backstage/plugin-catalog-backend';
 import {ScaffolderEntitiesProcessor} from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
 import {Router} from 'express';
 import {PluginEnvironment} from '../types';
-import {defaultUserTransformer, GithubOrgEntityProvider} from "@backstage/plugin-catalog-backend-module-github";
+import {
+    defaultUserTransformer, GithubEntityProvider,
+    GithubOrgEntityProvider,
+    GithubUser
+} from "@backstage/plugin-catalog-backend-module-github";
 
 export default async function createPlugin(
     env: PluginEnvironment,
@@ -17,7 +21,7 @@ export default async function createPlugin(
             frequency: {minutes: 60},
             timeout: {minutes: 15},
         }),
-        userTransformer: async (user, ctx) => {
+        userTransformer: async (user: GithubUser, ctx) => {
             const entity = await defaultUserTransformer(user, ctx);
             if (entity && user.organizationVerifiedDomainEmails?.length) {
                 entity.spec.profile!.email = user.organizationVerifiedDomainEmails[0]
@@ -25,7 +29,17 @@ export default async function createPlugin(
             return entity;
         }
     })
+
+    const githubEntityProvider = GithubEntityProvider.fromConfig(
+        env.config,
+        {
+            logger: env.logger,
+            scheduler: env.scheduler
+        }
+    )
+
     builder.addEntityProvider(githubOrgProvider);
+    builder.addEntityProvider(githubEntityProvider);
 
     const {processingEngine, router} = await builder.build();
     await processingEngine.start();
