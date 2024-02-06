2f8d16b144055647f6aed8c60c6a333019089cc7
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-02-06 15:33:02 +0100

    WIP

--- plugins/ros/src/components/ScenarioTable/ScenarioTable2.tsx
+++ plugins/ros/src/components/ScenarioTable/ScenarioTable2.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Collapse,
+  Grid,
   IconButton,
   Paper,
   Table,
@@ -7,7 +9,6 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TableRow,
   Typography,
 } from '@material-ui/core';
 import { ROS, Scenario } from '../interface/interfaces';
@@ -15,6 +16,8 @@ import { DeleteButton, EditButton } from './ScenarioTableButtons';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { InfoCard } from '@backstage/core-components';
+import Box from '@material-ui/core/Box';
+import TableRow from '@mui/material/TableRow';
 
 interface ScenarioTableProps {
   ros: ROS;
@@ -28,11 +31,11 @@ export const ScenarioTable = ({
   editRow,
 }: ScenarioTableProps) => {
   const Row = ({ scenario }: { scenario: Scenario }) => {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(true);
 
     return (
       <>
-        <TableRow key={scenario.ID}>
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
           <TableCell>
             <IconButton
               aria-label="expand row"
@@ -50,52 +53,69 @@ export const ScenarioTable = ({
             <DeleteButton onClick={() => deleteRow(scenario.ID)} />
           </TableCell>
         </TableRow>
-        {/*
         <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <TableContainer>
-              <Table>
-                <TableBody>
-                  <TableRow>
-                    <TableCell
-                      style={{ paddingBottom: 0, paddingTop: 0 }}
-                      colSpan={6}
-                    >
-                      <div style={{ display: open ? 'block' : 'none' }}>
-                        <TableCell>
-                          {scenario.trusselaktører.join(', ')}
-                        </TableCell>
-                        <TableCell>{scenario.sårbarheter.join(', ')}</TableCell>
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Grid container style={{ padding: '1rem' }}>
+                <Grid item xs={6}>
+                  <Typography variant="h6">Trusselaktører</Typography>
+                  <Box>{scenario.trusselaktører.join(', ')}</Box>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="h6">Tiltak</Typography>
+                  <TableContainer>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Beskrivelse</TableCell>
+                        <TableCell>Status</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {scenario.tiltak.map(tiltak => (
+                        <TableRow>
+                          <TableCell>{tiltak.beskrivelse}</TableCell>
+                          <TableCell>{tiltak.status}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </TableContainer>
+                </Grid>
+              </Grid>
+            </Collapse>
           </TableCell>
         </TableRow>
-        */}
       </>
     );
   };
 
   return (
     <InfoCard>
-      <Typography variant="h2">Scenarioer</Typography>
-      <TableContainer component={Paper} style={{ border: '1px solid #ffffff' }}>
+      <Typography variant="h3" style={{ paddingBottom: '1rem' }}>
+        Scenarioer
+      </Typography>
+      <TableContainer component={Paper}>
         <Table>
           <TableHead>
-            <TableRow>
+            <TableRow
+              style={{
+                borderBottom: '1px solid #ffffff',
+                borderTop: '1px solid #ffffff',
+              }}
+            >
               <TableCell />
-              {columns.map(column => (
-                <TableCell>{column}</TableCell>
-              ))}
+              <TableCell style={{ fontWeight: 'bold' }}>Beskrivelse</TableCell>
+              <TableCell align="center" style={{ fontWeight: 'bold' }}>
+                Konsekvens
+              </TableCell>
+              <TableCell align="center" style={{ fontWeight: 'bold' }}>
+                Sannsynlighet
+              </TableCell>
               <TableCell />
             </TableRow>
           </TableHead>
           <TableBody>
             {ros.scenarier.map(scenario => (
-              <Row scenario={scenario} />
+              <Row key={scenario.ID} scenario={scenario} />
             ))}
           </TableBody>
         </Table>
@@ -103,11 +123,3 @@ export const ScenarioTable = ({
     </InfoCard>
   );
 };
-
-const columns = [
-  'Beskrivelse',
-  // 'Trusselaktør',
-  // 'Sårbarhet',
-  'Konsekvens',
-  'Sannsynlighet',
-];
