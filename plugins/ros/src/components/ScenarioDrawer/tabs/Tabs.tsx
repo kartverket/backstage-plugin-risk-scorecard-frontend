aff229d326fc08829a1e8404ac0282f8c3437659
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-02-07 10:39:37 +0100

    USe tabs in drawer

---
+++ plugins/ros/src/components/ScenarioDrawer/tabs/Tabs.tsx
@@ -0,0 +1,44 @@
+import TabList from '@material-ui/lab/TabList/TabList';
+import { Tab, Typography } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import React from 'react';
+
+interface TabsProps {
+  setTab: (tab: string) => void;
+}
+
+export const Tabs = ({ setTab }: TabsProps) => {
+  return (
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <TabList
+        onChange={(_: any, newValue: string) => setTab(newValue)}
+        variant={'fullWidth'}
+      >
+        <Tab
+          label={
+            <Typography variant="subtitle1" style={{ fontWeight: 'bold' }}>
+              Konsekvens
+            </Typography>
+          }
+          value="konsekvens"
+        />
+        <Tab
+          label={
+            <Typography variant="subtitle1" style={{ fontWeight: 'bold' }}>
+              Sannsynlighet
+            </Typography>
+          }
+          value="sannsynlighet"
+        />
+        <Tab
+          label={
+            <Typography variant="subtitle1" style={{ fontWeight: 'bold' }}>
+              Tiltak
+            </Typography>
+          }
+          value="tiltak"
+        />
+      </TabList>
+    </Box>
+  );
+};
