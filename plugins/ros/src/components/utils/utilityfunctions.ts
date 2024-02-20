c8fb240d9150f793c4fde9de39bd20f5f929b17c
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-02-20 15:19:21 +0100

    Delete unused code#

--- plugins/ros/src/components/utils/utilityfunctions.ts
+++ plugins/ros/src/components/utils/utilityfunctions.ts
@@ -1,14 +1,4 @@
-import { GithubRepoInfo, ROS, TableData } from '../interface/interfaces';
-
-export const mapToTableData = (data: ROS): TableData[] =>
-  data.scenarier.map(scenario => ({
-    beskrivelse: scenario.beskrivelse,
-    trussel: scenario.trusselaktører.join(', '),
-    sårbarhet: scenario.sårbarheter.join(', '),
-    konsekvens: scenario.risiko.konsekvens,
-    sannsynlighet: scenario.risiko.sannsynlighet,
-    id: scenario.ID,
-  }));
+import { GithubRepoInfo } from '../interface/interfaces';
 
 export const uriToFetchRosIds = (
   baseUrl: string,
