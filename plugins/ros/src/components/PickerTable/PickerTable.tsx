8cb19667d9d2d11a6403e00da46b41af692e43e1
fosso <bragefosso@hotmail.com>
2024-02-23 16:24:37 +0100

    Updated content of the table. Changed spacing and font sizings

--- plugins/ros/src/components/PickerTable/PickerTable.tsx
+++ plugins/ros/src/components/PickerTable/PickerTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Radio, TableCell, makeStyles } from '@material-ui/core';
+import { Radio, TableCell, Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
   grid: {
@@ -30,47 +30,41 @@ export const PickerTable = ({
 }: PickerTableProps) => {
   const data = [
     [
-      'Varig og alvorlig økonomisk tap',
-      'Retten til personvern krenkes på en svært alvorlig måte',
       'Død eller varige alvorlige helsemssige konsekvenser',
-      'Den registrerte og samfunnet taper tilliten til Kartverket',
-      'Varig og alvorlig tap av omdømme',
+      'Varig og alvorlig økonomisk tap',
+      'N/A',
+      'N/A',
     ],
     [
-      'Økonomisk tap av betydelig varighet',
-      'Retten til personvern krenkes alvorlig i en lengre periode og involverer særlige kategorier/sårbare grupper',
       'Varig eller alvorlig helsemessige konsekvenser',
-      'Den registrerte taper tilliten til kartverket',
-      'Varig eller alvorlig tap av omdømme',
+      'Økonomisk tap av betydelig varighet for Kartverket og evt. tredejparter',
+      'Retten til personvern krenkes på en svært alvorlig måte',
+      'N/A',
     ],
     [
+      'Midlertidig eller mindre alvorlige helsemessige konsekvenser',
       'Økonomisk tap av noe varighet',
-      'Retten til personvern krenkes i en lengre persiode eller involverer særlige kategorier/sårbare grupper',
-      'Midlertidig eller noe mer alvorlige helsemessige konsekvenser for den registrerte',
-      'Den registrertes tillit til Kartverket utfordres',
-      'Midlertidig eller noe mer alvorlig tap av omdømme',
+      'Retten til personvern krenkes alvorlig i en lengre periode og involverer særlige kategorier/sårbare grupper',
+      'Varig negativ oppmerksomhet i nasjonale og internasjonale medier som kan redusere tillit',
     ],
     [
+      'N/A',
       'Forbigående økonomisk tap',
-      'Retten til personvern utfordres i en kort periode elller uten å involvere særlige kateogier/sårbare grupper',
-      'Midlertidig eller mindre alvorlige helsemessige konsekvenser for den registrerte.',
-      'Den registrertes tillit til Kartverket utfordres midlertidig',
-      'Midlertidig eller begrenset tap av omdømme',
+      'Retten til personvern krenkes i en lengre periode eller involverer særlige kategorier/sårbare grupper',
+      'Negative saker i nasjonale medier som kan redusere tillit',
     ],
     [
+      'N/A',
       'Forbigående mindre økonomisk tap',
-      'Retten til personvern utfordres i en svært kort periode elller uten å involvere særlige kateogier/sårbare grupper',
-      'Midlertidig eller mindre helsemessige konsekvenser for den registrerte',
-      'Den registrertes tillit til Kartverket utfordres i en kort periode',
-      'Midlertidig og begrenset tap av omdømme',
+      'Retten til personvern utfordres i en  svært kort periode og involverer ikke særlige kategorier/sårbare grupper',
+      'Midlertidig omdømmetap og liten innvirkning på tillit',
     ],
   ];
   const tableHeader = [
+    'Liv og helse',
     'Økonomisk',
     'Personvern',
-    'Liv og helse',
-    'Tillit',
-    'Omdømme',
+    'Omdømme og tillitt',
   ];
 
   const classes = useStyles();
@@ -80,35 +74,44 @@ export const PickerTable = ({
   };
 
   return (
-    <table className={classes.grid}>
-      <tbody>
-        <tr>
-          <TableCell className={classes.firstCell} />
-          {tableHeader.map((header, index) => (
-            <TableCell key={index} className={classes.firstCell}>
-              {header}
-            </TableCell>
-          ))}
-        </tr>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <TableCell className={classes.firstCell}>
-              <div className={classes.label}>
-                <span>{5 - rowIndex}</span>
-                <Radio
-                  checked={selectedValue === 5 - rowIndex}
-                  onChange={handleChangeRow(rowIndex)}
-                />
-              </div>
-            </TableCell>
-            {row.map((cell, cellIndex) => (
-              <TableCell key={cellIndex} className={classes.cell}>
-                {cell}
+    <>
+      <Typography style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+        Hvor alvorlig er den potensielle konsekvensen i det relevante området?
+        Hvis konsekvensen er relevant for flere områder gjelder det høyeste
+        konsekvensnivået.
+      </Typography>
+      <table className={classes.grid}>
+        <tbody>
+          <tr>
+            <TableCell className={classes.firstCell} />
+            {tableHeader.map((header, index) => (
+              <TableCell key={index} className={classes.firstCell}>
+                <Typography variant="subtitle1" style={{ fontWeight: 'bold' }}>
+                  {header}
+                </Typography>
               </TableCell>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <TableCell className={classes.firstCell}>
+                <div className={classes.label}>
+                  <Typography>{5 - rowIndex}</Typography>
+                  <Radio
+                    checked={selectedValue === 5 - rowIndex}
+                    onChange={handleChangeRow(rowIndex)}
+                  />
+                </div>
+              </TableCell>
+              {row.map((cell, cellIndex) => (
+                <TableCell key={cellIndex} className={classes.cell}>
+                  {cell}
+                </TableCell>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
