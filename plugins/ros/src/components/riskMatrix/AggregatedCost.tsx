3f2a810a77b097aee45573206402628f6c7544f6
fosso <bragefosso@hotmail.com>
2024-02-26 16:17:57 +0100

    refactor styling

--- plugins/ros/src/components/riskMatrix/AggregatedCost.tsx
+++ plugins/ros/src/components/riskMatrix/AggregatedCost.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Grid, IconButton, Typography } from '@material-ui/core';
+import { Grid, IconButton, Typography, makeStyles } from '@material-ui/core';
 import InfoIcon from '@mui/icons-material/Info';
 import { formatNOK } from '../utils/utilityfunctions';
 import { ROS } from '../utils/types';
@@ -11,25 +11,30 @@ interface AggregatedCostProps {
   onClose: () => void;
 }
 
+const useStyles = makeStyles({
+  gridOuterContainer: {
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  gridInnerContainer: {
+    justifyContent: 'center',
+    paddingBottom: '1rem',
+    paddingTop: '0.5rem',
+  },
+});
+
 export const AggregatedCost = ({ ros }: AggregatedCostProps) => {
   const cost = ros.scenarier
     .map(scenario => scenario.risiko.sannsynlighet * scenario.risiko.konsekvens)
     .reduce((a, b) => a + b, 0);
 
   const [showDialog, setShowDialog] = useState(false);
-
+  const classes = useStyles();
   return (
     <>
-      <Grid alignItems="center" container direction="column">
+      <Grid container className={classes.gridOuterContainer}>
         <Typography>Risikoen har en potensiell årlig kostnad på</Typography>
-        <Grid
-          container
-          direction="row"
-          alignContent="center"
-          justifyContent="center"
-          alignItems="center"
-          style={{ paddingBottom: '1rem', paddingTop: '0.5rem' }}
-        >
+        <Grid container className={classes.gridInnerContainer}>
           <Typography align="center" variant="h5">
             {formatNOK(cost)} kr
           </Typography>
