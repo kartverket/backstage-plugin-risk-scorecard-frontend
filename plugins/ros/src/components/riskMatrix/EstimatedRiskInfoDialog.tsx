14eaa77f5fd092aa394b1c32933ef6a0d1b72190
Brage Fosso <Fosso@users.noreply.github.com>
2024-03-07 16:22:25 +0100

    Change of content (#117)

--- plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
+++ plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
@@ -8,7 +8,7 @@ import {
   makeStyles,
 } from '@material-ui/core';
 
-export interface CostInfoDialogProps {
+export interface EstimatedRiskInfoDialogProps {
   isOpen: boolean;
   onClose: () => void;
 }
@@ -26,29 +26,29 @@ const useStyles = makeStyles({
   },
 });
 
-export const CostInfoDialog = ({ isOpen, onClose }: CostInfoDialogProps) => {
+export const EstimatedRiskInfoDialog = ({
+  isOpen,
+  onClose,
+}: EstimatedRiskInfoDialogProps) => {
   const classes = useStyles();
 
   return (
     <Dialog open={isOpen} onClose={onClose} maxWidth="xs">
       <Paper className={classes.paper}>
-        <DialogTitle className={classes.title}>
-          Potensiell årlig kostnad
-        </DialogTitle>
+        <DialogTitle className={classes.title}>Estimert risiko</DialogTitle>
         <DialogContent className={classes.text}>
           <DialogContentText className={classes.text}>
-            Den potensielle årlige kostnaden er basert på hvor stor risiko de
-            forskjellige scenariene utgjør. Hvis det er stor sannsynlighet for
-            at en alvorlig konsekvens skjer er det høy risiko for at det kan bli
-            en stor kostnad for Kartverket. Kostnaden er m.a.o. et forsøk på å
-            konkretisere verdien av risiko og er summen av den potensielle
-            årlige kostnaden for alle risikoscenariene i denne ROS-analysen.
+            Den estimerte risikoen er basert på hvor stor risiko de forskjellige
+            scenariene utgjør. Hvis det er stor sannsynlighet for at en alvorlig
+            konsekvens skjer er det høy risiko for at det kan bli en stor
+            kostnad for Kartverket. Kostnaden er m.a.o. et forsøk på å
+            konkretisere verdien av risiko og er summen av den estimerte
+            risikoen for alle risikoscenariene i denne ROS-analysen.
           </DialogContentText>
-          <b>Hvordan regner vi ut potensiell årlig kostnad?</b>
+          <b>Hvordan regner vi ut estimert risiko?</b>
           <DialogContentText className={classes.text}>
             Konsekvensen måles i kroner per hendelse og sannsynlighet måles i
-            hendelser per år. Den potensielle årlige kostnaden for risikoen blir
-            da: K x S
+            hendelser per år. Den estimerte risikoen blir da: K x S.
           </DialogContentText>
           <b>Konsekvens (kr/hendelse)</b>
           <DialogContentText className={classes.text}>
@@ -68,9 +68,9 @@ export const CostInfoDialog = ({ isOpen, onClose }: CostInfoDialogProps) => {
           </DialogContentText>
 
           <DialogContentText className={classes.text}>
-            F.eks: Et risikoscenarie med konsekvens 2 og sannsynlighet 4 har en
-            risiko for en årlig kostnad på 30 000 kr/hendelse x 50 hendelser/år
-            = 1 500 000 kr/år
+            Et risikoscenarie med konsekvens 2 og sannsynlighet 4 har en
+            estimert risiko på 30 000 kr/hendelse x 50 hendelser/år = 1 500 000
+            kr/år.
           </DialogContentText>
         </DialogContent>
       </Paper>
