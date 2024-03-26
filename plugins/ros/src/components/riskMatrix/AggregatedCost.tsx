48f5cce8fe67aa438c601b65e30162c667cb1d4e
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-03-26 16:50:59 +0100

    Format numbers as words (#143)

--- plugins/ros/src/components/riskMatrix/AggregatedCost.tsx
+++ plugins/ros/src/components/riskMatrix/AggregatedCost.tsx
@@ -1,51 +1,65 @@
-import React, { useState } from 'react';
-import { Box, IconButton, makeStyles, Typography } from '@material-ui/core';
+import React, {useState} from 'react';
+import {Box, IconButton, makeStyles, Typography} from '@material-ui/core';
 import InfoIcon from '@mui/icons-material/Info';
-import { formatNOK } from '../utils/utilityfunctions';
-import { ROS } from '../utils/types';
-import { EstimatedRiskInfoDialog } from './EstimatedRiskInfoDialog';
+import {ROS} from '../utils/types';
+import {EstimatedRiskInfoDialog} from './EstimatedRiskInfoDialog';
+import {formatNOK} from "../utils/utilityfunctions";
 
 interface AggregatedCostProps {
-  ros: ROS;
-  startRisiko: boolean;
+    ros: ROS;
+    startRisiko: boolean;
 }
 
 const useStyles = makeStyles({
-  outerBox: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-  },
-  innerBox: {
-    display: 'flex',
-    alignItems: 'center',
-  },
+    outerBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+    },
+    innerBox: {
+        display: 'flex',
+        alignItems: 'center',
+    },
 });
 
-export const AggregatedCost = ({ ros, startRisiko }: AggregatedCostProps) => {
-  const cost = ros.scenarier
-    .map(scenario =>
-      startRisiko
-        ? scenario.risiko.sannsynlighet * scenario.risiko.konsekvens
-        : scenario.restrisiko.sannsynlighet * scenario.restrisiko.konsekvens,
-    )
-    .reduce((a, b) => a + b, 0);
+export const AggregatedCost = ({ros, startRisiko}: AggregatedCostProps) => {
+    const cost = ros.scenarier
+        .map(scenario =>
+            startRisiko
+                ? scenario.risiko.sannsynlighet * scenario.risiko.konsekvens
+                : scenario.restrisiko.sannsynlighet * scenario.restrisiko.konsekvens,
+        )
+        .reduce((a, b) => a + b, 0);
 
-  const [showDialog, setShowDialog] = useState(false);
-  const { outerBox, innerBox } = useStyles();
-  return (
-    <Box className={outerBox}>
-      <Typography>Estimert risiko</Typography>
-      <Box className={innerBox}>
-        <Typography variant="h5">{formatNOK(cost)} kr/år</Typography>
-        <IconButton size="small" onClick={() => setShowDialog(true)}>
-          <InfoIcon />
-        </IconButton>
-      </Box>
-      <EstimatedRiskInfoDialog
-        isOpen={showDialog}
-        onClose={() => setShowDialog(false)}
-      />
-    </Box>
-  );
+    const [showDialog, setShowDialog] = useState(false);
+    const {outerBox, innerBox} = useStyles();
+    return (
+        <Box className={outerBox}>
+            <Typography>Estimert risiko</Typography>
+            <Box className={innerBox}>
+                <Typography variant="h5">{formatNumber(cost)} kr/år</Typography>
+                <IconButton size="small" onClick={() => setShowDialog(true)}>
+                    <InfoIcon/>
+                </IconButton>
+            </Box>
+            <EstimatedRiskInfoDialog
+                isOpen={showDialog}
+                onClose={() => setShowDialog(false)}
+            />
+        </Box>
+    );
 };
+
+function formatNumber(cost: number): string {
+    if (cost < 1e4) {
+        return formatNOK(cost);
+    } else if (cost < 1e6) {
+        return `${cost / 1e3} tusen`;
+    } else if (cost < 1e9) {
+        return `${cost / 1e6} ${cost / 1e6 === 1 ? 'million' : 'millioner'}`;
+    } else if (cost < 1e12) {
+        return `${cost / 1e9} ${cost / 1e9 === 1 ? 'milliard' : 'milliarder'}`;
+    } else {
+        return `${cost / 1e12} ${cost / 1e12 === 1 ? 'billion' : 'billioner'}`;
+    }
+}
