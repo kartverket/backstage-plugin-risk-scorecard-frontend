47f8008ab17bf222e37b74f41e923c3b7942c40a
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-04-03 17:28:17 +0200

    This probably works

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -57,7 +57,7 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
-import { RosPage } from '@internal/plugin-ros';
+import { RosPage } from '@bekk-spire/plugin-ros';
 
 const techdocsContent = (
   <EntityTechdocsContent>
