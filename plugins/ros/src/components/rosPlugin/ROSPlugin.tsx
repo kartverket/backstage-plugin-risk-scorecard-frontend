059aced60d1064c6929bc20cf0e7ff4d5459aa92
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-04-08 09:17:54 +0200

    Create wizard

--- plugins/ros/src/components/rosPlugin/ROSPlugin.tsx
+++ plugins/ros/src/components/rosPlugin/ROSPlugin.tsx
@@ -3,10 +3,28 @@ import { useFetchRoses, useScenarioDrawer } from '../utils/hooks';
 import { useLoadingStyles } from './rosPluginStyle';
 import { useFontStyles } from '../scenarioDrawer/style';
 import { useParams } from 'react-router';
-import { ROSDialogStates } from '../rosDialog/ROSDialog';
+import { ROSDialog, ROSDialogStates } from '../rosDialog/ROSDialog';
 import { Route, Routes } from 'react-router-dom';
 import { rosRouteRef, scenarioRouteRef } from '../../routes';
-import { ScenarioStepper } from '../scenarioStepper/ScenarioStepper';
+import {
+  ScenarioStepper,
+  ScenarioStepperSteps,
+} from '../scenarioStepper/ScenarioStepper';
+import { ScenarioDrawer } from '../scenarioDrawer/ScenarioDrawer';
+import { RiskMatrix } from '../riskMatrix/RiskMatrix';
+import { Button, CircularProgress, Grid, Typography } from '@material-ui/core';
+import { ScenarioTable } from '../scenarioTable/ScenarioTable';
+import { ROSInfo } from '../rosInfo/ROSInfo';
+import { ScenarioContext } from './ScenarioContext';
+import Alert from '@mui/material/Alert';
+import { getAlertSeverity } from '../utils/utilityfunctions';
+import {
+  Content,
+  ContentHeader,
+  SupportButton,
+} from '@backstage/core-components';
+import { Dropdown } from '../utils/Dropdown';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 export const ROSPlugin = () => {
   return (
@@ -29,6 +47,9 @@ const Plugin = () => {
   const openEditRosDialog = () => setROSDialogState(ROSDialogStates.Edit);
   const closeRosDialog = () => setROSDialogState(ROSDialogStates.Closed);
 
+  const [scenarioWizardStep, setScenarioWizardStep] =
+    useState<ScenarioStepperSteps | null>(null);
+
   const {
     selectedROS,
     roses,
@@ -48,86 +69,93 @@ const Plugin = () => {
 
   const classes = useLoadingStyles();
   const { button } = useFontStyles();
+  return (
+    <>
+      <ScenarioContext.Provider value={scenario}>
+        {response && (
+          <Alert severity={getAlertSeverity(response.status)}>
+            <Typography>{response.statusMessage}</Typography>
+          </Alert>
+        )}
 
-  return <ScenarioStepper />;
-  /* (<>
-     <ScenarioContext.Provider value={scenario}>
-       {response && (
-         <Alert severity={getAlertSeverity(response.status)}>
-           <Typography>{response.statusMessage}</Typography>
-         </Alert>
-       )}
+        <Content>
+          {scenarioWizardStep !== null ? (
+            <ScenarioStepper
+              step={scenarioWizardStep}
+              setStep={setScenarioWizardStep}
+            />
+          ) : (
+            <>
+              <ContentHeader title="Risiko- og sårbarhetsanalyse">
+                <SupportButton>Kul plugin ass!</SupportButton>
+              </ContentHeader>
 
-       <Content>
-         <ContentHeader title="Risiko- og sårbarhetsanalyse">
-           <SupportButton>Kul plugin ass!</SupportButton>
-         </ContentHeader>
+              {isFetching && (
+                <div className={classes.container}>
+                  <CircularProgress className={classes.spinner} size={80} />
+                </div>
+              )}
 
-         {isFetching && (
-           <div className={classes.container}>
-             <CircularProgress className={classes.spinner} size={80} />
-           </div>
-         )}
+              <Grid container spacing={3}>
+                {roses !== null && roses.length !== 0 && (
+                  <Grid item xs={3}>
+                    <Dropdown<string>
+                      options={roses.map(ros => ros.content.tittel) ?? []}
+                      selectedValues={selectedROS?.content.tittel ?? ''}
+                      handleChange={title => selectRos(title)}
+                      variant="standard"
+                    />
+                  </Grid>
+                )}
 
-         <Grid container spacing={3}>
-           {roses !== null && roses.length !== 0 && (
-             <Grid item xs={3}>
-               <Dropdown<string>
-                 options={roses.map(ros => ros.content.tittel) ?? []}
-                 selectedValues={selectedROS?.content.tittel ?? ''}
-                 handleChange={title => selectRos(title)}
-                 variant="standard"
-               />
-             </Grid>
-           )}
+                {!isFetching && (
+                  <Grid item xs>
+                    <Button
+                      startIcon={<AddCircleOutlineIcon />}
+                      variant="text"
+                      color="primary"
+                      onClick={openCreateRosDialog}
+                      className={button}
+                    >
+                      Opprett ny analyse
+                    </Button>
+                  </Grid>
+                )}
 
-           {!isFetching && (
-             <Grid item xs>
-               <Button
-                 startIcon={<AddCircleOutlineIcon />}
-                 variant="text"
-                 color="primary"
-                 onClick={openCreateRosDialog}
-                 className={button}
-               >
-                 Opprett ny analyse
-               </Button>
-             </Grid>
-           )}
+                {selectedROS && (
+                  <>
+                    <Grid item xs={12}>
+                      <ROSInfo
+                        ros={selectedROS}
+                        approveROS={approveROS}
+                        edit={openEditRosDialog}
+                      />
+                    </Grid>
+                    <Grid item xs={8}>
+                      <ScenarioTable ros={selectedROS.content} />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <RiskMatrix ros={selectedROS.content} />
+                    </Grid>
+                  </>
+                )}
+              </Grid>
+            </>
+          )}
 
-           {selectedROS && (
-             <>
-               <Grid item xs={12}>
-                 <ROSInfo
-                   ros={selectedROS}
-                   approveROS={approveROS}
-                   edit={openEditRosDialog}
-                 />
-               </Grid>
-               <Grid item xs={8}>
-                 <ScenarioTable ros={selectedROS.content} />
-               </Grid>
-               <Grid item xs={4}>
-                 <RiskMatrix ros={selectedROS.content} />
-               </Grid>
-             </>
-           )}
-         </Grid>
+          {ROSDialogState !== ROSDialogStates.Closed && (
+            <ROSDialog
+              onClose={closeRosDialog}
+              createNewRos={createNewROS}
+              updateRos={updateROS}
+              ros={selectedROS}
+              dialogState={ROSDialogState}
+            />
+          )}
 
-         {ROSDialogState !== ROSDialogStates.Closed && (
-           <ROSDialog
-             onClose={closeRosDialog}
-             createNewRos={createNewROS}
-             updateRos={updateROS}
-             ros={selectedROS}
-             dialogState={ROSDialogState}
-           />
-         )}
-
-         <ScenarioDrawer />
-       </Content>
-     </ScenarioContext.Provider>
-   </>
- );
- */
+          <ScenarioDrawer />
+        </Content>
+      </ScenarioContext.Provider>
+    </>
+  );
 };
--- plugins/ros/src/components/scenarioStepper/ScenarioStepper.tsx
+++ plugins/ros/src/components/scenarioStepper/ScenarioStepper.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Step, StepLabel, Stepper } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
-const steps = ['Scenario', 'Startrisiko', 'Tiltak', 'Restrisiko'];
+const ScenarioStepperSteps = [
+  'Scenario',
+  'Startrisiko',
+  'Tiltak',
+  'Restrisiko',
+] as const;
 
-export const ScenarioStepper = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
+export type ScenarioStepperSteps = (typeof ScenarioStepperSteps)[number];
 
+interface ScenarioStepperProps {
+  step: ScenarioStepperSteps;
+  setStep: (step: ScenarioStepperSteps | null) => void;
+}
+
+export const ScenarioStepper = ({ step, setStep }: ScenarioStepperProps) => {
   return (
-    <Stepper activeStep={activeStep}>
-      {steps.map((label, index) => (
-        <Step key={label}>
-          <StepLabel>{label}</StepLabel>
-        </Step>
-      ))}
-    </Stepper>
+    step && (
+      <Box>
+        <Stepper
+          activeStep={ScenarioStepperSteps.indexOf(step)}
+          style={{ background: 'transparent' }}
+        >
+          {ScenarioStepperSteps.map(label => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+      </Box>
+    )
   );
 };
