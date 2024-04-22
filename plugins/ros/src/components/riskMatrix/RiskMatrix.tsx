10beafb3710ebfc0990671436cfcef70ce58d8da
Brage Fosso <Fosso@users.noreply.github.com>
2024-04-22 15:13:32 +0200

    Removed applied duplicate fontstyling from risk matrix (#192)

--- plugins/ros/src/components/riskMatrix/RiskMatrix.tsx
+++ plugins/ros/src/components/riskMatrix/RiskMatrix.tsx
@@ -44,7 +44,7 @@ export const RiskMatrix = ({ ros }: { ros: ROS }) => {
         <Box className={gridWrapper}>
           <Box className={grid}>
             <Box className={konsekvens}>
-              <Typography className={`${centered} ${text} ${label2}`}>
+              <Typography className={`${centered} ${label2}`}>
                 {t('dictionary.consequence')}
               </Typography>
             </Box>
@@ -76,7 +76,7 @@ export const RiskMatrix = ({ ros }: { ros: ROS }) => {
               </Box>
             ))}
             <Box className={sannsynlighet}>
-              <Typography className={`${centered} ${text} ${label2}`}>
+              <Typography className={`${centered} ${label2}`}>
                 {t('dictionary.probability')}
               </Typography>
             </Box>
