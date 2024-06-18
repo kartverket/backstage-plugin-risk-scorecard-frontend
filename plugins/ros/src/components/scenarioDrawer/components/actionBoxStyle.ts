b72faff368a38b224e4ccdf82d4bfcfda8d8c30e
fosso <bragefosso@hotmail.com>
2024-06-18 11:04:23 +0200

    Existing actions in drawer. Rearrange items in Actionbox

--- plugins/ros/src/components/scenarioDrawer/components/actionBoxStyle.ts
+++ plugins/ros/src/components/scenarioDrawer/components/actionBoxStyle.ts
@@ -1,12 +1,34 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useActionBoxStyles = makeStyles(_ => ({
+export const useActionBoxStyles = makeStyles(theme => ({
   alignCenter: {
     display: 'flex',
     alignItems: 'center',
   },
   justifyEnd: {
     display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: theme.spacing(1),
+  },
+  column: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  paddingTop: {
+    paddingTop: theme.spacing(2),
+  },
+  actionDescription: {
+    marginTop: theme.spacing(-2),
+  },
+  chipDropdown: {
+    display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'flex-end',
   },
 }));
