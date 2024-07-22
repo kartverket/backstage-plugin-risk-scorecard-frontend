086a5ce45a65a1c20319da1d1416a7b8eda71564
Tollefsen <havard.tollefsen@gmail.com>
2024-07-22 15:52:42 +0200

    use correct translation

--- plugins/ros/src/components/scenarioDrawer/components/ActionFormSection.tsx
+++ plugins/ros/src/components/scenarioDrawer/components/ActionFormSection.tsx
@@ -78,7 +78,7 @@ const ActionFormSection = ({
                 {...register(`actions.${index}.url`, { 
                   pattern: {
                     value: /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-                    message: 'er dette en url?',
+                    message: t('scenarioDrawer.action.urlError'),
                     }
                   })
                 }
