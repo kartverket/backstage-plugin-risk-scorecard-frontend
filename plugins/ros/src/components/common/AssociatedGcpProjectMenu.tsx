7d87732ad4a335212c7adfac2e2621e6b5746cfe
Lars Sørensen <larssorensen98@hotmail.com>
2024-11-13 15:47:23 +0100

    WIP

--- plugins/ros/src/components/common/AssociatedGcpProjectMenu.tsx
+++ plugins/ros/src/components/common/AssociatedGcpProjectMenu.tsx
@@ -1,95 +1,31 @@
-import {useAuthenticatedFetch} from "../../utils/hooks";
-import Box from "@mui/material/Box";
-import React, {useEffect, useState} from "react";
-import Typography from "@mui/material/Typography";
-import {useTranslationRef} from "@backstage/core-plugin-api/alpha";
-import {pluginRiScTranslationRef} from "../../utils/translations";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import {Menu, SelectChangeEvent, styled} from "@mui/material";
-import {gcpProjectIdToReadableString} from "../../utils/utilityfunctions";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import {EnhancedEncryptionOutlined} from "@material-ui/icons";
-import EditButton from "./EditButton";
-
+import React from 'react';
+import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
+import { pluginRiScTranslationRef } from '../../utils/translations';
+import { styled } from '@mui/material';
+import { gcpProjectIdToReadableString } from '../../utils/utilityfunctions';
+import EditButton from './EditButton';
 
 export const AssociatedGcpProjectMenu = () => {
-    const {t} = useTranslationRef(pluginRiScTranslationRef)
-    const {
-        fetchAssociatedGcpProjects
-    } = useAuthenticatedFetch()
+  const { t } = useTranslationRef(pluginRiScTranslationRef);
+  const ButtonStyledDiv = styled('div')(({ theme }) => ({
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+  }));
 
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const [associatedGcpProjects, setAssociatedGcpProjects] = useState<string[]>(
-        [],
-    );
-    const [chosenGcpProject, setChosenGcpProject] = useState<string>('');
-
-    const handleChangeGcpProject = (item: string) => {
-        setChosenGcpProject(item);
-        handleClose()
-    };
-
-    useEffect(() => {
-        fetchAssociatedGcpProjects()
-            .then(res => {
-                setAssociatedGcpProjects(res);
-                setChosenGcpProject(res[0]);
-            })
-            .catch(err => {
-                throw err;
-            });
-    }, []);
-
-    const ButtonStyledDiv = styled('div')(({ theme }) => ({
-        ...theme.typography.button,
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(1),
-    }));
-
-    return (
-        <ButtonStyledDiv
-            sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                gap: 1,
-            }}
-        >
-                {`${t('associatedGcpProject.description')}: ${gcpProjectIdToReadableString(chosenGcpProject)}`}
-            <EditButton />
-        </ButtonStyledDiv>
-    );
-}
-
-// <Menu
-//     anchorEl={anchorEl}
-//     open={open}
-//     onClose={handleClose}
-//     anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'left',
-//     }}
-//     transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'left',
-//     }}
-// >
-//     {associatedGcpProjects.map((item, _) => (
-//         <MenuItem
-//             value={item}
-//             onClick={() => handleChangeGcpProject(item)}
-//         >
-//             {gcpProjectIdToReadableString(item)}
-//         </MenuItem>
-//     ))}
-// </Menu>
+  return (
+    <ButtonStyledDiv
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 1,
+      }}
+    >
+      {`${t(
+        'associatedGcpProject.description',
+      )}: ${gcpProjectIdToReadableString(chosenGcpProject)}`}
+      <EditButton />
+    </ButtonStyledDiv>
+  );
+};
--- plugins/ros/src/components/sopsConfigDialog/NewSopsConfigDialog.tsx
+++ plugins/ros/src/components/sopsConfigDialog/NewSopsConfigDialog.tsx
@@ -1,16 +1,71 @@
 import Dialog from "@mui/material/Dialog";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import DialogTitle from "@mui/material/DialogTitle";
 import {useTranslationRef} from "@backstage/core-plugin-api/alpha";
 import {pluginRiScTranslationRef} from "../../utils/translations";
+import FormLabel from "@mui/material/FormLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import LinearProgress from "@mui/material/LinearProgress";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import {gcpProjectIdToReadableString} from "../../utils/utilityfunctions";
+import {useAuthenticatedFetch} from "../../utils/hooks";
 
 export const NewSopsConfigDialog = () => {
     const { t } = useTranslationRef(pluginRiScTranslationRef);
 
+    const {
+        fetchAssociatedGcpProjects
+    } = useAuthenticatedFetch()
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const [associatedGcpProjects, setAssociatedGcpProjects] = useState<string[]>(
+        [],
+    );
+    const [chosenGcpProject, setChosenGcpProject] = useState<string>('');
+
+    const handleChangeGcpProject = (item: string) => {
+        setChosenGcpProject(item);
+        handleClose()
+    };
+
+    useEffect(() => {
+        fetchAssociatedGcpProjects()
+            .then(res => {
+                setAssociatedGcpProjects(res);
+                setChosenGcpProject(res[0]);
+            })
+            .catch(err => {
+                throw err;
+            });
+    }, []);
+
     return (
         <Dialog open={true}>
             <DialogTitle>{t('newSopsConfigDialog.title')}</DialogTitle>
-
+            <FormLabel>{t('newSopsConfigDialog.description')}</FormLabel>
+            <Select
+                value={chosenGcpProject}
+                required
+                {...register('gcpProjectId', {
+                    required: true,
+                    onChange: handleChangeGcpProject,
+                })}
+                error={errors.gcpProjectId !== undefined}
+            >
+                {associatedGcpProjects.map((item, _) => (
+                    <MenuItem value={item}>
+                        {gcpProjectIdToReadableString(item)}
+                    </MenuItem>
+                ))}
+            </Select>
         </Dialog>
     );
 }
