afbd8ddccd66b3e7ff1b2b2a1a6ac525e29be902
Lars Sørensen <larssorensen98@hotmail.com>
2024-12-09 15:06:20 +0100

    Runned prettier

--- plugins/ros/src/components/sopsConfigDialog/GitBranchMenu.tsx
+++ plugins/ros/src/components/sopsConfigDialog/GitBranchMenu.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Badge from '@mui/material/Badge';
 import Menu from '@mui/material/Menu';
 import Tooltip from '@mui/material/Tooltip';
