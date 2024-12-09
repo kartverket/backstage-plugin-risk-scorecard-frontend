29368158af0d62bb51db4c56225678d923bee294
Lars Sørensen <larssorensen98@hotmail.com>
2024-12-09 13:22:28 +0100

    Update eslint configuration

--- plugins/ros/.eslintrc.js
+++ plugins/ros/.eslintrc.js
@@ -1,10 +1,8 @@
-/* eslint-disable */
 const eslintFactory = require('@backstage/cli/config/eslint-factory');
-
 module.exports = {
-    ...eslintFactory(__dirname),
-    rules: {
-        'react/jsx-boolean-value': 'off',
-    },
+  ...eslintFactory(__dirname),
+  rules: {
+    // Disable the react/jsx-boolean-value rule
+    'react/jsx-boolean-value': 'off',
+  },
 };
-/* eslint-enable */
