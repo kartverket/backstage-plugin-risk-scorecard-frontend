218c45252f79d95a8ca7356a96defa3ea41bf013
Ingelin Sundnes <86956132+ingelinsu@users.noreply.github.com>
2025-04-16 08:37:33 +0200

    🐛  fix for dark theme markdown (#540)
    * bug fix for dark theme
    
    * fix linting error
    
    * remove unnecessary comment
    
    * standarize text colors for dark theme and light theme
    
    * fix linting
    
    * diabled=false

--- plugins/ros/src/components/common/Markdown.tsx
+++ plugins/ros/src/components/common/Markdown.tsx
@@ -1,15 +1,26 @@
 import Typography from '@mui/material/Typography';
 import ReactMarkdown from 'react-markdown';
+import { useTheme } from '@mui/material/styles';
 import 'github-markdown-css/github-markdown.css';
 import remarkBreaks from 'remark-breaks';
+import { commonTextColor, commonBackgroundColor } from '../../utils/style';
 
 type Props = {
   description: string;
+  disabled?: boolean;
 };
 
-export function Markdown({ description }: Props) {
+export function Markdown({ description, disabled = false }: Props) {
+  const theme = useTheme();
+
   return (
-    <Typography className="markdown-body">
+    <Typography
+      className="markdown-body"
+      sx={{
+        color: commonTextColor(theme, disabled),
+        backgroundColor: commonBackgroundColor(theme, disabled),
+      }}
+    >
       <ReactMarkdown remarkPlugins={[remarkBreaks]}>
         {description}
       </ReactMarkdown>
