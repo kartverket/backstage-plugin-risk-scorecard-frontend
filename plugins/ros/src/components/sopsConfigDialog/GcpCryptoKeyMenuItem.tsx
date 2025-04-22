1acb03d684ca79f74af34cec637ff4e51c4e3920
Ingelin Sundnes <86956132+ingelinsu@users.noreply.github.com>
2025-04-22 12:59:42 +0200

    🪄 update design edit encryption (#543)
    * add border to key and age key, hide addd new public age key
    
    * chosen key has check mark instead of key icon
    
    * add expand icon
    
    * remove unused
    
    * add margintop to add public key button
    
    * edit --> settings icon
    
    * remove key ring info
    
    * remove check icon, but add blue color to the chosen key

--- plugins/ros/src/components/sopsConfigDialog/GcpCryptoKeyMenuItem.tsx
+++ plugins/ros/src/components/sopsConfigDialog/GcpCryptoKeyMenuItem.tsx
@@ -1,18 +1,16 @@
 import MenuItem from '@mui/material/MenuItem';
-
 import { VpnKey } from '@mui/icons-material';
 import { GcpCryptoKeyObject } from '../../utils/DTOs';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import { Avatar, Tooltip } from '@mui/material';
+import { Avatar } from '@mui/material';
 import ListItemText from '@mui/material/ListItemText';
-import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
-import { pluginRiScTranslationRef } from '../../utils/translations';
 
 interface GcpCryptoKeyMenuItemProps {
   value: string;
   gcpCryptoKey: GcpCryptoKeyObject;
   handleClick: (gcpCryptoKey: GcpCryptoKeyObject) => void;
   hasAccess: boolean;
+  isSelected: boolean;
 }
 
 export function GcpCryptoKeyMenuItem({
@@ -20,35 +18,34 @@ export function GcpCryptoKeyMenuItem({
   gcpCryptoKey,
   handleClick,
   hasAccess,
+  isSelected,
 }: GcpCryptoKeyMenuItemProps) {
-  const { t } = useTranslationRef(pluginRiScTranslationRef);
-
   return (
-    <Tooltip
-      title={t('sopsConfigDialog.gcpKeyDoNotHaveAccessDescription')}
-      disableHoverListener={hasAccess}
+    <MenuItem
+      value={value}
+      onClick={() => handleClick(JSON.parse(value) as GcpCryptoKeyObject)}
+      disabled={!hasAccess}
+      sx={{
+        backgroundColor: isSelected ? 'inherit' : undefined,
+        '&:hover': {
+          backgroundColor: isSelected ? 'inherit' : 'grey.100',
+        },
+      }}
     >
-      <MenuItem
-        value={value}
-        onClick={() => handleClick(JSON.parse(value) as GcpCryptoKeyObject)}
-        disabled={!hasAccess}
-      >
-        <ListItemAvatar>
-          <Avatar>
-            <VpnKey />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={gcpCryptoKey.keyName}
-          secondary={
-            <>
-              Project ID: {gcpCryptoKey.projectId}
-              <br />
-              Key ring: {gcpCryptoKey.keyRing}
-            </>
-          }
-        />
-      </MenuItem>
-    </Tooltip>
+      <ListItemAvatar>
+        <Avatar
+          sx={{
+            backgroundColor: isSelected ? 'primary.main' : 'default',
+            color: isSelected ? 'white' : 'inherit',
+          }}
+        >
+          <VpnKey />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={gcpCryptoKey.keyName}
+        secondary={<>Project ID: {gcpCryptoKey.projectId}</>}
+      />
+    </MenuItem>
   );
 }
