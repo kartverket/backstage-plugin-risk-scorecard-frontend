3fbdbd171d860e119d6f86bdeb0cde55f66fc6d3
Ask Røsand <askrosand@gmail.com>
2025-09-10 15:16:13 +0200

    added remix icons (the icons used by backstage UI)

--- plugins/ros/src/PluginRoot.tsx
+++ plugins/ros/src/PluginRoot.tsx
@@ -6,6 +6,7 @@ import { ScenarioProvider } from './contexts/ScenarioContext';
 import { riScRouteRef, scenarioRouteRef } from './routes';
 import { RiScProvider } from './contexts/RiScContext';
 import '@backstage/ui/css/styles.css';
+import 'remixicon/fonts/remixicon.css';
 
 const emotionInsertionPoint = document.createElement('meta');
 emotionInsertionPoint.setAttribute('name', 'emotion-insertion-point');
