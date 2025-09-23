b186c499be2cbae08f0c6142e37fe8c779f6276f
Martin Bolle <martin.bolle@bekk.no>
2025-09-23 08:40:14 +0200

    Updated button display in dialog window

--- plugins/ros/src/components/common/mixins.ts
+++ plugins/ros/src/components/common/mixins.ts
@@ -1,6 +1,9 @@
 import { SxProps, Theme } from '@mui/material/styles';
 
 export const dialogActions: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   gap: 1,
   paddingTop: 0,
   paddingX: 3,
