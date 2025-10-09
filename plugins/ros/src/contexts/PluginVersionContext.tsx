fe9b0859de5a2aadb1f8c749e76731ee94d76207
Ask Røsand <askrosand@gmail.com>
2025-10-09 10:35:30 +0200

    read package version from package.json instead of hardcoded value

--- plugins/ros/src/contexts/PluginVersionContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-const PLUGIN_VERSION_NUMBER = '3.6.0'; // Should always match the version specified in package.json
-export const PluginVersionContext = createContext(PLUGIN_VERSION_NUMBER);
