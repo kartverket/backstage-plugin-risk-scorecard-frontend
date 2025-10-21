94ba0cebd54fa492d7389a373d346fc96f4ba911
Ask Røsand <askrosand@gmail.com>
2025-10-21 10:40:34 +0200

    Use dialog elements to avoid duplicated background

--- plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
+++ plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
@@ -1,5 +1,4 @@
 import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
-import { Dialog } from '@material-ui/core';
 import { consequenceOptions, probabilityOptions } from '../../utils/constants';
 import { pluginRiScTranslationRef } from '../../utils/translations';
 import {
@@ -7,7 +6,10 @@ import {
   formatNOK,
   probabilityIndexToTranslationKeys,
 } from '../../utils/utilityfunctions';
-import { Card, CardBody, CardHeader, Flex, Text } from '@backstage/ui';
+import { Flex, Text } from '@backstage/ui';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
 
 interface EstimatedRiskInfoDialogProps {
   isOpen: boolean;
@@ -21,99 +23,99 @@ export function EstimatedRiskInfoDialog({
   const { t } = useTranslationRef(pluginRiScTranslationRef);
 
   return (
-    <Dialog open={isOpen} onClose={onClose} maxWidth="xs">
-      <Card
-        style={{
-          padding: '16px',
-          margin: 0,
-        }}
-      >
-        <CardHeader>
-          <Text variant="title-x-small" weight="bold">
-            {t('infoDialog.title')}
-          </Text>
-        </CardHeader>
-        <CardBody>
-          <Flex gap="2" direction="column">
-            <Text>{t('infoDialog.description')}</Text>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.calculatedHowTitle')}
-              </Text>
-              <Text>
-                {' '}
-                {t('infoDialog.calculatedHow')}
-                <sup>{t('infoDialog.calculatedHowExponent')}</sup>
-                {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
-              </Text>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.consequenceTitle')}
-              </Text>
-              <Flex direction="column" gap="1">
-                {consequenceOptions.map((option, index) => (
-                  <Text key={index}>
-                    <b>{index + 1}</b>: 20<sup>{index + 3}</sup> ={' '}
-                    {formatNOK(option)} {t('infoDialog.consequenceUnit')} ={' '}
-                    <b>
-                      {t(consequenceIndexToTranslationKeys[index] as any, {})}
-                    </b>
-                  </Text>
-                ))}
-              </Flex>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.probabilityTitle')}
-              </Text>
-              <Flex direction="column" gap="1">
-                {probabilityOptions.map((option, index) => (
-                  <Text key={index}>
-                    <b>{index + 1}</b>: 20<sup>{index - 2}</sup> = {option}{' '}
-                    {t('infoDialog.probabilityUnit')} ={' '}
-                    <b>
-                      {t(probabilityIndexToTranslationKeys[index] as any, {})}
-                    </b>
-                    <br />
-                  </Text>
-                ))}
-              </Flex>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('dictionary.example')}
-              </Text>
-              <Text>
-                {t('infoDialog.example.part1')}
-                <b>2</b> (20<sup>4</sup> = 160,000{' '}
-                {t('infoDialog.consequenceUnit')})
-                {t('infoDialog.example.part2')}
-                <b>4</b> (20<sup>1</sup> = 20 {t('infoDialog.probabilityUnit')})
-                {t('infoDialog.example.part3')}
-                20<sup>1+4-1</sup> = 160,000{' '}
-                {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
-              </Text>
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      maxWidth="xs"
+      style={{
+        padding: '16px',
+        margin: 0,
+      }}
+    >
+      <DialogTitle>
+        <Text variant="title-x-small" weight="bold">
+          {t('infoDialog.title')}
+        </Text>
+      </DialogTitle>
+      <DialogContent>
+        <Flex gap="2" direction="column">
+          <Text>{t('infoDialog.description')}</Text>
+          <Flex direction="column" gap="0">
+            <Text
+              variant="body-large"
+              weight="bold"
+              style={{ marginTop: '8px' }}
+            >
+              {t('infoDialog.calculatedHowTitle')}
+            </Text>
+            <Text>
+              {' '}
+              {t('infoDialog.calculatedHow')}
+              <sup>{t('infoDialog.calculatedHowExponent')}</sup>
+              {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
+            </Text>
+          </Flex>
+          <Flex direction="column" gap="0">
+            <Text
+              variant="body-large"
+              weight="bold"
+              style={{ marginTop: '8px' }}
+            >
+              {t('infoDialog.consequenceTitle')}
+            </Text>
+            <Flex direction="column" gap="1">
+              {consequenceOptions.map((option, index) => (
+                <Text key={index}>
+                  <b>{index + 1}</b>: 20<sup>{index + 3}</sup> ={' '}
+                  {formatNOK(option)} {t('infoDialog.consequenceUnit')} ={' '}
+                  <b>
+                    {t(consequenceIndexToTranslationKeys[index] as any, {})}
+                  </b>
+                </Text>
+              ))}
             </Flex>
           </Flex>
-        </CardBody>
-      </Card>
+          <Flex direction="column" gap="0">
+            <Text
+              variant="body-large"
+              weight="bold"
+              style={{ marginTop: '8px' }}
+            >
+              {t('infoDialog.probabilityTitle')}
+            </Text>
+            <Flex direction="column" gap="1">
+              {probabilityOptions.map((option, index) => (
+                <Text key={index}>
+                  <b>{index + 1}</b>: 20<sup>{index - 2}</sup> = {option}{' '}
+                  {t('infoDialog.probabilityUnit')} ={' '}
+                  <b>
+                    {t(probabilityIndexToTranslationKeys[index] as any, {})}
+                  </b>
+                  <br />
+                </Text>
+              ))}
+            </Flex>
+          </Flex>
+          <Flex direction="column" gap="0">
+            <Text
+              variant="body-large"
+              weight="bold"
+              style={{ marginTop: '8px' }}
+            >
+              {t('dictionary.example')}
+            </Text>
+            <Text>
+              {t('infoDialog.example.part1')}
+              <b>2</b> (20<sup>4</sup> = 160,000{' '}
+              {t('infoDialog.consequenceUnit')}){t('infoDialog.example.part2')}
+              <b>4</b> (20<sup>1</sup> = 20 {t('infoDialog.probabilityUnit')})
+              {t('infoDialog.example.part3')}
+              20<sup>1+4-1</sup> = 160,000{' '}
+              {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
+            </Text>
+          </Flex>
+        </Flex>
+      </DialogContent>
     </Dialog>
   );
 }
