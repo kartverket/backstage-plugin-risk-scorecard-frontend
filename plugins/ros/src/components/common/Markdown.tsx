8c11e9d3aa75bd53e2894ee9f4d808bd014de59b
Ask Røsand <askrosand@gmail.com>
2025-10-23 10:36:42 +0200

    fixed dark/light mdoe for markdown field. Matches look before bumping backstage

--- plugins/ros/src/components/common/Markdown.tsx
+++ plugins/ros/src/components/common/Markdown.tsx
@@ -2,14 +2,25 @@ import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css/github-markdown.css';
 import remarkBreaks from 'remark-breaks';
 import { Text } from '@backstage/ui';
+import { useTheme } from '@mui/material/styles';
 
 type Props = {
   description: string;
 };
 
 export function Markdown({ description }: Props) {
+  const theme = useTheme();
   return (
-    <Text variant="body-large">
+    <Text
+      variant="body-large"
+      className="markdown-body"
+      style={{
+        color:
+          theme.palette.mode === 'dark'
+            ? 'var(--bui-white)'
+            : 'var(--bui-black)',
+      }}
+    >
       {' '}
       <ReactMarkdown remarkPlugins={[remarkBreaks]}>
         {description}
