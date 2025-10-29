0b41213a973f778d43cab750e9b69974cbb1d26f
Martin Bolle <martin.bolle@bekk.no>
2025-10-29 13:02:29 +0100

    Ensure tabbing moves focus to texfield

--- plugins/ros/src/components/common/MarkdownInput.tsx
+++ plugins/ros/src/components/common/MarkdownInput.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState, useEffect } from 'react';
+import { forwardRef, useState, useEffect, useRef } from 'react';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormLabel from '@mui/material/FormLabel';
@@ -48,13 +48,31 @@ export const MarkdownInput = forwardRef<HTMLDivElement, Props>(
       onMarkdownChange?.(newValue || '');
     };
 
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+    const onFocusCapture = (element: React.FocusEvent<HTMLDivElement>) => {
+      if (element.relatedTarget) {
+        const root = element.currentTarget as HTMLDivElement;
+        const target = root.querySelector(
+          'textarea, [contenteditable="true"]',
+        ) as HTMLElement | null;
+        if (target) {
+          setTimeout(() => target.focus());
+        }
+      }
+    };
+
     return (
       <FormControl sx={formControlStyles} error={error}>
         {label && <FormLabel sx={formLabel}>{label}</FormLabel>}
         {sublabel && (
           <FormHelperText sx={formHelperText}>{sublabel}</FormHelperText>
         )}
-        <div data-color-mode={theme.palette.mode}>
+        <div
+          data-color-mode={theme.palette.mode}
+          ref={wrapperRef}
+          onFocusCapture={onFocusCapture}
+        >
           <MDEditor
             ref={ref}
             value={markdownContent}
