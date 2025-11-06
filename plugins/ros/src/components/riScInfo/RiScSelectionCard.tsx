bd46b791190677c0d1168a68e248e1d8370c27ed
Kristian Johannessen <kristian.johannessen@bekk.no>
2025-11-06 09:29:35 +0100

    Set first available risc as selected in drop down after deletion of risc

--- plugins/ros/src/components/riScInfo/RiScSelectionCard.tsx
+++ plugins/ros/src/components/riScInfo/RiScSelectionCard.tsx
@@ -22,6 +22,7 @@ export function RiScSelectionCard(props: Props) {
         {riScs !== null && riScs.length !== 0 && (
           <>
             <Select
+              value={selectedRiSc?.id}
               className={styles.selectTrigger}
               aria-label={t('contentHeader.multipleRiScs')}
               options={riScs.map(riSc => ({
@@ -31,7 +32,6 @@ export function RiScSelectionCard(props: Props) {
               onSelectionChange={key => {
                 if (key) selectRiSc(key.toString());
               }}
-              defaultSelectedKey={selectedRiSc?.id ?? ''}
               size="medium"
             />
           </>
