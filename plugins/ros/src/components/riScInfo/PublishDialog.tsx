e44649fcc136e6188bd8ec1c0e2f4291a24a85f2
Martin Bolle <70632679+martinbolle@users.noreply.github.com>
2025-12-08 14:51:44 +0100

    Create dialog component (#824)
    * created a separate and reusable DialogComponent
    
    * Adding the new component to the files where we use dialogs
    
    * Replaced last two Dialogs with reusable component
    
    * Removed dialogFooter from component + cleanUp
    
    ---------
    
    Co-authored-by: Martin Bolle <martin.bolle@bekk.no>

--- plugins/ros/src/components/riScInfo/PublishDialog.tsx
+++ plugins/ros/src/components/riScInfo/PublishDialog.tsx
@@ -8,15 +8,9 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import { DifferenceFetchState } from '../../utils/types';
 import { pluginRiScTranslationRef } from '../../utils/translations';
 import { RiScDifferenceDialog } from './riScStatus/RiScDifferenceDialog';
-import {
-  Flex,
-  Text,
-  Dialog,
-  DialogTrigger,
-  DialogHeader,
-  DialogBody,
-} from '@backstage/ui';
+import { Flex } from '@backstage/ui';
 import styles from './RiScSelectionCard.module.css';
+import DialogComponent from '../dialog/DialogComponent';
 
 interface RiScPublishDialogProps {
   openDialog: boolean;
@@ -42,58 +36,50 @@ export const RiScPublishDialog = ({
   }
 
   return (
-    <DialogTrigger>
-      <Dialog
-        isOpen={openDialog}
-        onOpenChange={handleCancel}
-        className={styles.riScInfoDialog}
-      >
-        <DialogHeader className={styles.riscSelectionDialogHeader}>
-          <Text variant="title-small" weight="bold">
-            {isDeletion
-              ? t('publishDialog.titleDelete')
-              : t('publishDialog.titleUpdate')}
-          </Text>
-        </DialogHeader>
-        <DialogBody className={styles.riscSelectionDialogBody}>
-          <>
-            {!isDeletion && (
-              <RiScDifferenceDialog
-                differenceFetchState={differenceFetchState}
+    <DialogComponent
+      isOpen={openDialog}
+      onClick={handleCancel}
+      header={
+        isDeletion
+          ? t('publishDialog.titleDelete')
+          : t('publishDialog.titleUpdate')
+      }
+      className={styles.riScInfoDialog}
+    >
+      <>
+        {!isDeletion && (
+          <RiScDifferenceDialog differenceFetchState={differenceFetchState} />
+        )}
+        <Alert severity="info" icon={false}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={riskOwnerApproves}
+                onChange={handleCheckboxInput}
               />
-            )}
-            <Alert severity="info" icon={false}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    checked={riskOwnerApproves}
-                    onChange={handleCheckboxInput}
-                  />
-                }
-                label={
-                  isDeletion
-                    ? t('publishDialog.checkboxLabelDelete')
-                    : t('publishDialog.checkboxLabelUpdate')
-                }
-              />
-            </Alert>
-          </>
-          <Flex justify="between" pt="24px">
-            <Button variant="outlined" color="primary" onClick={handleCancel}>
-              {t('dictionary.cancel')}
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handlePublish}
-              disabled={!riskOwnerApproves}
-            >
-              {t('dictionary.confirm')}
-            </Button>
-          </Flex>
-        </DialogBody>
-      </Dialog>
-    </DialogTrigger>
+            }
+            label={
+              isDeletion
+                ? t('publishDialog.checkboxLabelDelete')
+                : t('publishDialog.checkboxLabelUpdate')
+            }
+          />
+        </Alert>
+      </>
+      <Flex justify="between" pt="24px">
+        <Button variant="outlined" color="primary" onClick={handleCancel}>
+          {t('dictionary.cancel')}
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handlePublish}
+          disabled={!riskOwnerApproves}
+        >
+          {t('dictionary.confirm')}
+        </Button>
+      </Flex>
+    </DialogComponent>
   );
 };
--- plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
+++ plugins/ros/src/components/riskMatrix/EstimatedRiskInfoDialog.tsx
@@ -6,15 +6,9 @@ import {
   formatNOK,
   probabilityIndexToTranslationKeys,
 } from '../../utils/utilityfunctions';
-import {
-  Flex,
-  Text,
-  DialogTrigger,
-  Dialog,
-  DialogHeader,
-  DialogBody,
-} from '@backstage/ui';
+import { Flex, Text } from '@backstage/ui';
 import styles from './Tabs.module.css';
+import DialogComponent from '../dialog/DialogComponent';
 
 interface EstimatedRiskInfoDialogProps {
   isOpen: boolean;
@@ -28,98 +22,69 @@ export function EstimatedRiskInfoDialog({
   const { t } = useTranslationRef(pluginRiScTranslationRef);
 
   return (
-    <DialogTrigger>
-      <Dialog
-        isOpen={isOpen}
-        onOpenChange={onClose}
-        className={styles.riskMatrixDialog}
-      >
-        <DialogHeader>
-          <Text variant="title-small" weight="bold">
-            {t('infoDialog.title')}
+    <DialogComponent
+      isOpen={isOpen}
+      onClick={onClose}
+      className={styles.riskMatrixDialog}
+      header={t('infoDialog.title')}
+    >
+      <Flex gap="2" direction="column">
+        <Text>{t('infoDialog.description')}</Text>
+        <Flex direction="column" gap="0">
+          <Text variant="body-large" weight="bold" style={{ marginTop: '8px' }}>
+            {t('infoDialog.calculatedHowTitle')}
           </Text>
-        </DialogHeader>
-        <DialogBody>
-          <Flex gap="2" direction="column">
-            <Text>{t('infoDialog.description')}</Text>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.calculatedHowTitle')}
+          <Text>
+            {' '}
+            {t('infoDialog.calculatedHow')}
+            <sup>{t('infoDialog.calculatedHowExponent')}</sup>
+            {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
+          </Text>
+        </Flex>
+        <Flex direction="column" gap="0">
+          <Text variant="body-large" weight="bold" style={{ marginTop: '8px' }}>
+            {t('infoDialog.consequenceTitle')}
+          </Text>
+          <Flex direction="column" gap="1">
+            {consequenceOptions.map((option, index) => (
+              <Text key={index}>
+                <b>{index + 1}</b>: 20<sup>{index + 3}</sup> ={' '}
+                {formatNOK(option)} {t('infoDialog.consequenceUnit')} ={' '}
+                <b>{t(consequenceIndexToTranslationKeys[index] as any, {})}</b>
               </Text>
-              <Text>
-                {' '}
-                {t('infoDialog.calculatedHow')}
-                <sup>{t('infoDialog.calculatedHowExponent')}</sup>
-                {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
-              </Text>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.consequenceTitle')}
-              </Text>
-              <Flex direction="column" gap="1">
-                {consequenceOptions.map((option, index) => (
-                  <Text key={index}>
-                    <b>{index + 1}</b>: 20<sup>{index + 3}</sup> ={' '}
-                    {formatNOK(option)} {t('infoDialog.consequenceUnit')} ={' '}
-                    <b>
-                      {t(consequenceIndexToTranslationKeys[index] as any, {})}
-                    </b>
-                  </Text>
-                ))}
-              </Flex>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('infoDialog.probabilityTitle')}
-              </Text>
-              <Flex direction="column" gap="1">
-                {probabilityOptions.map((option, index) => (
-                  <Text key={index}>
-                    <b>{index + 1}</b>: 20<sup>{index - 2}</sup> = {option}{' '}
-                    {t('infoDialog.probabilityUnit')} ={' '}
-                    <b>
-                      {t(probabilityIndexToTranslationKeys[index] as any, {})}
-                    </b>
-                    <br />
-                  </Text>
-                ))}
-              </Flex>
-            </Flex>
-            <Flex direction="column" gap="0">
-              <Text
-                variant="body-large"
-                weight="bold"
-                style={{ marginTop: '8px' }}
-              >
-                {t('dictionary.example')}
-              </Text>
-              <Text>
-                {t('infoDialog.example.part1')}
-                <b>2</b> (20<sup>4</sup> = 160,000{' '}
-                {t('infoDialog.consequenceUnit')})
-                {t('infoDialog.example.part2')}
-                <b>4</b> (20<sup>1</sup> = 20 {t('infoDialog.probabilityUnit')})
-                {t('infoDialog.example.part3')}
-                20<sup>1+4-1</sup> = 160,000{' '}
-                {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
-              </Text>
-            </Flex>
+            ))}
           </Flex>
-        </DialogBody>
-      </Dialog>
-    </DialogTrigger>
+        </Flex>
+        <Flex direction="column" gap="0">
+          <Text variant="body-large" weight="bold" style={{ marginTop: '8px' }}>
+            {t('infoDialog.probabilityTitle')}
+          </Text>
+          <Flex direction="column" gap="1">
+            {probabilityOptions.map((option, index) => (
+              <Text key={index}>
+                <b>{index + 1}</b>: 20<sup>{index - 2}</sup> = {option}{' '}
+                {t('infoDialog.probabilityUnit')} ={' '}
+                <b>{t(probabilityIndexToTranslationKeys[index] as any, {})}</b>
+                <br />
+              </Text>
+            ))}
+          </Flex>
+        </Flex>
+        <Flex direction="column" gap="0">
+          <Text variant="body-large" weight="bold" style={{ marginTop: '8px' }}>
+            {t('dictionary.example')}
+          </Text>
+          <Text>
+            {t('infoDialog.example.part1')}
+            <b>2</b> (20<sup>4</sup> = 160,000 {t('infoDialog.consequenceUnit')}
+            ){t('infoDialog.example.part2')}
+            <b>4</b> (20<sup>1</sup> = 20 {t('infoDialog.probabilityUnit')})
+            {t('infoDialog.example.part3')}
+            20<sup>1+4-1</sup> = 160,000{' '}
+            {t('riskMatrix.estimatedRisk.unit.nokPerYear')}.
+          </Text>
+        </Flex>
+      </Flex>
+    </DialogComponent>
   );
 }
