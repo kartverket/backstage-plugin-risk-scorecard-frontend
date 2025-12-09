16f73891fa7da3b433c44158bbf0a9403bdd0163
Ask Røsand <askrosand@gmail.com>
2025-12-09 15:07:05 +0100

    refactor: removed unused code

--- plugins/ros/src/utils/actions.ts
+++ plugins/ros/src/utils/actions.ts
@@ -3,7 +3,6 @@ import { ActionStatusOptions } from './constants.ts';
 import {
   calculateDaysSince,
   calculateUpdatedStatus,
-  UpdatedStatusEnumType,
   UpdatedStatusEnum,
 } from './utilityfunctions.ts';
 import { ProfileInfo } from '@backstage/core-plugin-api';
@@ -62,28 +61,6 @@ export function getActionsWithLastUpdated(
     }
   }
   return actionsWithLastUpdated;
-}
-
-export type ActionWithUpdatedStatus = Action & {
-  updatedStatus: UpdatedStatusEnumType;
-};
-
-export function getActionsWithUpdatedStatus(
-  actions: Action[],
-): ActionWithUpdatedStatus[] {
-  return actions.map(action => {
-    const daysSinceLastUpdate = action.lastUpdated
-      ? calculateDaysSince(new Date(action.lastUpdated))
-      : null;
-    const updatedStatus =
-      daysSinceLastUpdate !== null
-        ? calculateUpdatedStatus(daysSinceLastUpdate)
-        : UpdatedStatusEnum.VERY_OUTDATED;
-    return {
-      ...action,
-      updatedStatus: updatedStatus,
-    } as ActionWithUpdatedStatus;
-  });
 }
 
 export function getUpdatedStatus(action: Action) {
