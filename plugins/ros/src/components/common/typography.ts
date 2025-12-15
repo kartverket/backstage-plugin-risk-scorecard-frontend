a1283cf600ce87f6aea97fe8f7218148544eb507
Ask Røsand <askrosand@gmail.com>
2025-12-15 20:08:59 +0100

    refactor: removed need for label styling

--- plugins/ros/src/components/common/typography.ts
+++ plugins/ros/src/components/common/typography.ts
@@ -1,12 +1,5 @@
 import { SxProps, Theme } from '@mui/material/styles';
 
-export const label: SxProps<Theme> = {
-  fontSize: '0.875rem',
-  fontWeight: 700,
-  textTransform: 'uppercase',
-  paddingBottom: '0.4rem',
-};
-
 export const formLabel: SxProps<Theme> = {
   fontSize: '0.875rem',
   fontWeight: 700,
