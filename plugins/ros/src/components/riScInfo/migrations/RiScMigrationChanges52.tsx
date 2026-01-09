510292c5ea0e6adea24ffad1c41c18c037654c4e
annaaurora <anna-aurora@live.no>
2026-01-09 14:01:47 +0100

    show migration 5.2 explanation unconditionally

--- plugins/ros/src/components/riScInfo/migrations/RiScMigrationChanges52.tsx
+++ plugins/ros/src/components/riScInfo/migrations/RiScMigrationChanges52.tsx
@@ -1,26 +1,17 @@
-import { MigrationChanges52 } from '../../../utils/types.ts';
 import { MigrationTitle } from './components/MigrationTitle.tsx';
 import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
 import { pluginRiScTranslationRef } from '../../../utils/translations.ts';
 
-interface RiScMigrationChanges52Props {
-  changes: MigrationChanges52;
-}
-
-export function RiScMigrationChanges52({
-  changes,
-}: RiScMigrationChanges52Props) {
+export function RiScMigrationChanges52() {
   const { t } = useTranslationRef(pluginRiScTranslationRef);
-
-  const hasRemovedValuations = (changes.removedValuationsCount ?? 0) > 0;
-
-  if (!hasRemovedValuations) return null;
 
   return (
     <MigrationTitle
       from="5.1"
       to="5.2"
-      migrationExplanation={t('migrationDialog.migration52.changeExplanation')}
+      migrationExplanation={t(
+        'migrationDialog.migration52.changeExplanation',
+      )}
       changelogUrl="https://github.com/kartverket/backstage-plugin-risk-scorecard-backend/blob/main/docs/schemaChangelog.md#52"
     />
   );
