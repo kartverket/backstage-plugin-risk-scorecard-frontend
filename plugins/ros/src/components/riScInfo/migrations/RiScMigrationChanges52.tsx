96e2b1c7f3fc05de166dc9c5ebb226e0b7d5b5bc
annaaurora <anna-aurora@live.no>
2026-01-09 13:45:13 +0100

    simplify and modify so that changeExplanation is shown

--- plugins/ros/src/components/riScInfo/migrations/RiScMigrationChanges52.tsx
+++ plugins/ros/src/components/riScInfo/migrations/RiScMigrationChanges52.tsx
@@ -1,12 +1,7 @@
 import { MigrationChanges52 } from '../../../utils/types.ts';
-import { ChangeSetBox } from '../changeset/components/ChangeSetBox.tsx';
 import { MigrationTitle } from './components/MigrationTitle.tsx';
-import { ChangeSetTag } from '../changeset/components/ChangeSetTag.tsx';
-import { ChangeSetBoxTitle } from '../changeset/components/ChangeSetBoxTitle.tsx';
-import { ChangeSetTags } from '../changeset/components/ChangeSetTags.tsx';
 import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
 import { pluginRiScTranslationRef } from '../../../utils/translations.ts';
-import { ChangeSetChangedProperty } from '../changeset/components/ChangeSetChangedProperty.tsx';
 
 interface RiScMigrationChanges52Props {
   changes: MigrationChanges52;
@@ -17,40 +12,16 @@ export function RiScMigrationChanges52({
 }: RiScMigrationChanges52Props) {
   const { t } = useTranslationRef(pluginRiScTranslationRef);
 
-  const deletedValuations = changes.valuations;
-  if (!deletedValuations || deletedValuations.type !== 'DELETED') return null;
+  const hasRemovedValuations = (changes.removedValuationsCount ?? 0) > 0;
 
-  const count = Array.isArray(deletedValuations.oldValue)
-    ? deletedValuations.oldValue.length
-    : 0;
+  if (!hasRemovedValuations) return null;
 
   return (
-    <>
-      <MigrationTitle
-        from="5.1"
-        to="5.2"
-        migrationExplanation={t(
-          'migrationDialog.migration52.changeExplanation',
-        )}
-        changelogUrl="https://github.com/kartverket/backstage-plugin-risk-scorecard-backend/blob/main/docs/schemaChangelog.md#52"
-      />
-
-      <ChangeSetBox type="primary">
-        <ChangeSetTags>
-          <ChangeSetTag
-            type="primary"
-            text={t('migrationDialog.schemaVersion')}
-          />
-        </ChangeSetTags>
-
-        <ChangeSetBoxTitle title={t('migrationDialog.migration52.title')} />
-
-        <ChangeSetChangedProperty
-          propertyName={t('dictionary.valuation')}
-          oldValue={`${t('migrationDialog.migration52.oldValue')}: ${count}`}
-          newValue={t('migrationDialog.removed')}
-        />
-      </ChangeSetBox>
-    </>
+    <MigrationTitle
+      from="5.1"
+      to="5.2"
+      migrationExplanation={t('migrationDialog.migration52.changeExplanation')}
+      changelogUrl="https://github.com/kartverket/backstage-plugin-risk-scorecard-backend/blob/main/docs/schemaChangelog.md#52"
+    />
   );
 }
