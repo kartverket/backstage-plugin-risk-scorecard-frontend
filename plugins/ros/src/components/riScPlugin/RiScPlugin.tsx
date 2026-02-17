1bf4df5a76daf8a5509beddd856ec26f8d456ceb
Martin Bolle <70632679+martinbolle@users.noreply.github.com>
2026-02-17 15:22:55 +0100

    fix: enhance error handling for decryption failure (#891)
    Co-authored-by: Martin Bolle <martin.bolle@bekk.no>
    Co-authored-by: annaaurora <80164059+annaaurora@users.noreply.github.com>

--- plugins/ros/src/components/riScPlugin/RiScPlugin.tsx
+++ plugins/ros/src/components/riScPlugin/RiScPlugin.tsx
@@ -58,6 +58,8 @@ export function RiScPlugin() {
     response,
     updateStatus,
     riScs,
+    failedToFetchGcpCryptoKeys,
+    allRiScsFailedDecryption,
   } = useRiScs();
 
   const { t } = useTranslationRef(pluginRiScTranslationRef);
@@ -91,7 +93,8 @@ export function RiScPlugin() {
           {!isFetching &&
             riScs !== null &&
             riScs.length === 0 &&
-            !selectedRiSc && (
+            !selectedRiSc &&
+            !allRiScsFailedDecryption && (
               <Flex
                 justify="center"
                 align="center"
@@ -100,15 +103,16 @@ export function RiScPlugin() {
                 <FirstRiScDialog onNewRiSc={openCreateRiScDialog} />
               </Flex>
             )}
-          {!isFetching && riScs === null && (
-            <Flex
-              align="center"
-              justify="center"
-              className={riscStyles.componentLayout}
-            >
-              <ErrorState />
-            </Flex>
-          )}
+          {!isFetching &&
+            (failedToFetchGcpCryptoKeys || allRiScsFailedDecryption) && (
+              <Flex
+                align="center"
+                justify="center"
+                className={riscStyles.componentLayout}
+              >
+                <ErrorState />
+              </Flex>
+            )}
           {isFetching && <Spinner size={80} />}
 
           <Grid container spacing={4}>
