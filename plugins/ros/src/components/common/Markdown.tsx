6ca6e1fe114b10b13dd764ccead0f6b186bb059c
Ask R. <askrosand@gmail.com>
2026-02-25 15:53:16 +0100

    refactor: removed MUI useTheme

--- plugins/ros/src/components/common/Markdown.tsx
+++ plugins/ros/src/components/common/Markdown.tsx
@@ -2,23 +2,18 @@ import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css/github-markdown.css';
 import remarkBreaks from 'remark-breaks';
 import { Text } from '@backstage/ui';
-import { useTheme } from '@mui/material/styles';
 
 type Props = {
   description: string;
 };
 
 export function Markdown({ description }: Props) {
-  const theme = useTheme();
   return (
     <Text
       variant="body-large"
       className="markdown-body"
       style={{
-        color:
-          theme.palette.mode === 'dark'
-            ? 'var(--bui-white)'
-            : 'var(--bui-black)',
+        color: 'var(--bui-fg-primary)',
       }}
     >
       {' '}
