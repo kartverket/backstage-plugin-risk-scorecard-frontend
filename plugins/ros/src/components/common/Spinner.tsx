7d73f6f0700d5e1f78408e4b19fcccbd3acea5ea
Ask R. <askrosand@gmail.com>
2026-02-26 16:04:57 +0100

    refactor: css modules

--- plugins/ros/src/components/common/Spinner.tsx
+++ plugins/ros/src/components/common/Spinner.tsx
@@ -1,23 +1,11 @@
 import { Flex } from '@backstage/ui';
 import CircularProgress from '@mui/material/CircularProgress';
+import styles from './Spinner.module.css';
 
 export function Spinner({ size }: { size?: string | number }) {
   return (
-    <Flex
-      align="start"
-      justify="center"
-      style={{
-        minWidth: '100vw',
-        height: '100vh',
-      }}
-    >
-      <CircularProgress
-        size={size}
-        sx={{
-          display: 'flex',
-          marginTop: '200px',
-        }}
-      />
+    <Flex align="start" justify="center" className={styles.container}>
+      <CircularProgress size={size} className={styles.progress} />
     </Flex>
   );
 }
