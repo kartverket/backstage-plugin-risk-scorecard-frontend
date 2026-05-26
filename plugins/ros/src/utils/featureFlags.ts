d667e12411194ebae7fc2f484782057b16d28244
Kristian Veøy <kristian.veoy@bekk.no>
2026-05-26 12:37:15 +0200

    fix: Hide the system RiSc feature flag registration (#968)
    * Hide RiSc feature flag registration
    
    * fix: conventional commit trigger
    
    ---------
    
    Co-authored-by: Andreas Amundsen <andreas.amundsen123@gmail.com>

--- plugins/ros/src/utils/featureFlags.ts
+++ plugins/ros/src/utils/featureFlags.ts
@@ -1,6 +1,18 @@
-import { featureFlagsApiRef, useApi } from '@backstage/core-plugin-api';
+import {
+  featureFlagsApiRef,
+  type PluginFeatureFlagConfig,
+  useApi,
+} from '@backstage/core-plugin-api';
 
 export const systemRiScsFeatureFlag = 'system-riscs';
+
+export const riScFeatureFlags: PluginFeatureFlagConfig[] = [
+  // {
+  //   name: systemRiScsFeatureFlag,
+  //   description:
+  //     'Testing av system-RoS for Team SKVIS. Bruk på eget ansvar. UNNGÅ lagring av System-RoSer i åpne repoer.',
+  // },
+];
 
 export function useSystemRiScsFeatureFlag(): boolean {
   const featureFlagsApi = useApi(featureFlagsApiRef);
